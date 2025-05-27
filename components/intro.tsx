06e382ad3f8f1eb013bb137963253c0c686fbd04
Anittavinter <anittavinter@gmail.com>
2025-05-27 18:12:28 +0200

    error

--- components/intro.tsx
+++ components/intro.tsx
@@ -6,9 +6,9 @@ export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;I´m Vinter.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;m Vinter.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;UI/UX Designer and Developer based in Nairobi, Kenya. 
+        I&#39;m UI/UX Designer and Developer based in Nairobi, Kenya. 
           I help businesses connect with their audiences through intuitive, responsive digital experiences.
         </p>
       </div>
