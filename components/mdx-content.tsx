5320d38e39990bcf798e5db5be1ccaef4287fc09
Anittavinter <anittavinter@gmail.com>
2025-05-27 18:05:20 +0200

    Eslint Build

--- components/mdx-content.tsx
+++ components/mdx-content.tsx
@@ -1,18 +1,22 @@
-import { JSX } from 'react'
-import { highlight } from 'sugar-high'
-import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc'
+import { JSX, ReactNode, HTMLAttributes } from 'react';
+import { highlight } from 'sugar-high';
+import { MDXRemote, MDXRemoteProps } from 'next-mdx-remote/rsc';
 
-import Counter from '@/components/counter'
+import Counter from '@/components/counter';
 
-function Code({ children, ...props }: any) {
-  let codeHTML = highlight(children)
-  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />
+interface CodeProps extends HTMLAttributes<HTMLElement> {
+  children?: ReactNode;
+}
+
+function Code({ children, ...props }: CodeProps) {
+  const codeHTML = highlight(String(children ?? ''));
+  return <code dangerouslySetInnerHTML={{ __html: codeHTML }} {...props} />;
 }
 
 const components = {
   code: Code,
-  Counter
-}
+  Counter,
+};
 
 export default function MDXContent(
   props: JSX.IntrinsicAttributes & MDXRemoteProps
@@ -22,5 +26,5 @@ export default function MDXContent(
       {...props}
       components={{ ...components, ...(props.components || {}) }}
     />
-  )
+  );
 }
