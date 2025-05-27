2edf6bfa1370d47c0a72072f3a57dde1ac38b227
Anittavinter <anittavinter@gmail.com>
2025-05-27 11:21:19 +0200

    01: Order Up project

--- components/intro.tsx
+++ components/intro.tsx
@@ -1,22 +1,22 @@
 import Image from 'next/image'
 import authorImage from '@/public/images/authors/named.jpg'
 
+
 export default function Intro() {
   return (
     <section className='flex flex-col-reverse items-start gap-x-10 gap-y-4 pb-24 md:flex-row md:items-center'>
       <div className='mt-2 flex-1 md:mt-0'>
-        <h1 className='title no-underline'>Hey, I&#39;m Haron.</h1>
+        <h1 className='title no-underline'>Hey, I&#39;I´m Vinter.</h1>
         <p className='mt-3 font-light text-muted-foreground'>
-          I&#39;m a software engineer based in Nairobi, Kenya. I&#39;m
-          passionate about learning new technologies and sharing knowledge with
-          others.
+          I&#39;UI/UX Designer and Developer based in Nairobi, Kenya. 
+          I help businesses connect with their audiences through intuitive, responsive digital experiences.
         </p>
       </div>
       <div className='relative'>
         <Image
           className='flex-1 rounded-lg grayscale'
           src={authorImage}
-          alt='Haron Ngaira'
+          alt='Vinter Achieno'
           width={175}
           height={175}
           priority
